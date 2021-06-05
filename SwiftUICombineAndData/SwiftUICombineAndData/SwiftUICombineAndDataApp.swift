3a7ed993ba95fcc1582f77b12bb982ddf8c0f2fc
leemyeonji <61834038+leemyeonji@users.noreply.github.com>
2021-06-05 13:19:34 +0900

    First Commit !!

---
+++ SwiftUICombineAndData/SwiftUICombineAndData/SwiftUICombineAndDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICombineAndDataApp.swift
+//  SwiftUICombineAndData
+//
+//  Created by 임현지 on 2021/06/05.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICombineAndDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
