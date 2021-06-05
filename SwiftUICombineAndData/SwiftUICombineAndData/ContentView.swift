cbb82b520b717010e54e32eb5f83ac9a96c206db
leemyeonji <61834038+leemyeonji@users.noreply.github.com>
2021-06-05 14:05:40 +0900

    TrackableScrollView

--- SwiftUICombineAndData/SwiftUICombineAndData/ContentView.swift
+++ SwiftUICombineAndData/SwiftUICombineAndData/ContentView.swift
@@ -8,9 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var contentOffset = CGFloat(0)
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ZStack(alignment: .top) {
+                TrackableScrollView(offsetChanged: { offset in
+                    contentOffset = offset.y
+                }) {
+                    Text("Hello world")
+                        .padding()
+                }
+                VisualEffectBlur(blurStyle: .systemMaterial)
+                    .opacity(contentOffset < -16 ? 1 : 0)
+                    .animation(.easeIn)
+                    .ignoresSafeArea()
+                    .frame(height: 0)
+            }
+            .frame(maxHeight: .infinity, alignment: .top)
+            .background(AccountBackground())
+            .navigationBarHidden(true)
+        }
+        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
